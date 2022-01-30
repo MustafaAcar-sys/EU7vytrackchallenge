81a9627c084c80a368456a0d3d8dcccc59c92e24
OscarSah <oscar@cybertekschool.com>
2022-01-30 23:39:10 +0300

    First Week Commit

---
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.vytrack.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports"},
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@login"
+)
+public class CukesRunner {
+}
